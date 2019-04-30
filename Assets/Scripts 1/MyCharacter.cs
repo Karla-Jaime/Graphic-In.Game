e85e13c47a6846260b8765116748912a3b64f74c
Karla-Jaime <carlykhr@gmail.com>
2019-04-30 07:52:34 -0700

    Inicio
    i

---
+++ Assets/Scripts 1/MyCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacter : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    public float colorPower= 1f;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+               
+    }
+
+    private void update()
+    {
+        render.material.SetFloat("_ColorPower", colorPower);
+    }
+}
