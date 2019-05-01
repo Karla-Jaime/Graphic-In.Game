bbfc23b3049302d2d2eefef7ce8aa32db1d3b0ca
Karla-Jaime <carlykhr@gmail.com>
2019-04-30 19:12:49 -0700

    Slider(aun no sirve)
    a

--- Assets/Scripts 1/MyCharacter.cs
+++ Assets/Scripts 1/MyCharacter.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+///////
+using UnityEngine.UI;
+//Se agrega una liberia para poder controlar UI
 
 public class MyCharacter : MonoBehaviour
 {
     Renderer render;
     [SerializeField]
     public float colorPower= 1f;
+
+    //Para mostrar una variable en el inspector de Unity
+    [SerializeField]
+    Slider sld_colorPower;
 
     private void Start()
     {
@@ -18,4 +25,5 @@ public class MyCharacter : MonoBehaviour
     {
         render.material.SetFloat("_ColorPower", colorPower);
     }
+
 }
